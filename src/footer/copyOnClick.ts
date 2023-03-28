8eb68101bd07a6b22bf7a4b21c582bb4ae98b61b
MatiasDelorenzi <matiasdelorenzi1997@gmail.com>
2023-03-28 11:03:31 -0300

    Copy on click footer animation

---
+++ src/footer/copyOnClick.ts
@@ -0,0 +1,36 @@
+const emailDefault: HTMLElement = document.querySelector('.cc-footer-email-default')
+const emailCopied: HTMLElement = document.querySelector('.cc-footer-email-copied')
+
+function fadeInElement(element: HTMLElement) {
+    element.style.opacity = '0'
+    const fadeEffect = setInterval(() => {
+        if (element.style.opacity != "1") {
+            element.style.opacity = `${Number(element.style.opacity) + 0.1}`
+        } else {
+            clearInterval(fadeEffect)
+        }
+    }, 25)
+}
+
+function fadeOutElement(elementOut: HTMLElement, elementIn: HTMLElement) {
+    elementOut.style.opacity = '1'
+    const fadeEffect = setInterval(() => {
+        if (elementOut.style.opacity != "0") {
+            elementOut.style.opacity = `${Number(elementOut.style.opacity) - 0.1}`
+        } else {
+            elementOut.style.display = 'none'
+            clearInterval(fadeEffect)
+            elementIn.style.display = 'flex'
+            fadeInElement(elementIn)
+        }
+    }, 25)
+}
+
+emailDefault.addEventListener('click', () => {
+    navigator.clipboard.writeText(emailDefault.innerText).then(() => {
+        fadeOutElement(emailDefault, emailCopied)
+        setTimeout(() => {
+            fadeOutElement(emailCopied, emailDefault)
+        }, 1500)
+    })
+})
