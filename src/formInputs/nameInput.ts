0e2cce6b03916184453183483d96879c8160cca4
MatiasDelorenzi <matiasdelorenzi1997@gmail.com>
2023-04-03 10:08:04 -0300

    Add underline animation for input

--- src/formInputs/nameInput.ts
+++ src/formInputs/nameInput.ts
@@ -1,43 +1,59 @@
-export const nameInput: HTMLInputElement = document.querySelector('.cc-name-input') as HTMLInputElement
+export const nameInput: HTMLInputElement = document.querySelector('.name-input')
 export let isNameErrorDisplayed: boolean = false
-const emailInputBelow: HTMLInputElement = document.querySelector('.cc-email-input') as HTMLInputElement
+const nameInputUnderline: HTMLElement = document.querySelector('.name-underline')
+const nameInputContainer: HTMLElement = document.querySelector('.project-input-container')
+const emailInputBelow: HTMLElement = document.querySelector('.cc-email-input')
 const nameRequiredErrorMessage: HTMLElement = document.querySelector('.cc-name-required-error')
 
 const emailInputStyles: CSSStyleDeclaration = getComputedStyle(emailInputBelow)
 const nameRequiredErrorStyles: CSSStyleDeclaration = getComputedStyle(nameRequiredErrorMessage)
 const emailInputMarginTop: string = emailInputStyles.marginTop.substring(0, emailInputStyles.marginTop.length - 2)
 const nameRequiredErrorLineHeight: string = nameRequiredErrorStyles.lineHeight.substring(0, nameRequiredErrorStyles.lineHeight.length - 2)
-
-nameInput.style.border = '1px solid transparent'
-nameInput.style.borderBottom = '1px solid #7b7b7b'
+nameInputUnderline.style.maxWidth = `${nameInputContainer.getBoundingClientRect().width}px`
 
 export const displayNameRequiredError = (): void => {
     if (!isNameErrorDisplayed) {
+        nameInputUnderline.style.width = '100%'
+        nameInputContainer.style.marginBottom = "0px"
         emailInputBelow.style.marginTop = `${Number(emailInputMarginTop) - Number(nameRequiredErrorLineHeight)}px`
         nameRequiredErrorMessage.style.display = 'block'
-        nameInput.style.borderBottom = '1px solid #cb0b0b'
+        nameInputUnderline.style.backgroundColor = '#cb0b0b'
         isNameErrorDisplayed = true
     }
 }
 
 const hideNameRequiredError = (): void => {
-    nameInput.style.border = '1px solid transparent'
-    nameInput.style.borderBottom = '1px solid #7b7b7b'
+    nameInputUnderline.style.backgroundColor = "#7b7b7b"
     emailInputBelow.style.marginTop = `${emailInputMarginTop}px`
     nameRequiredErrorMessage.style.display = 'none'
     isNameErrorDisplayed = false
 }
 
+nameInput.addEventListener('focus', () => {
+    if (nameInput.value === "") {
+        nameInput.placeholder = ''
+    }
+})
+
 nameInput.addEventListener('blur', () => {
-    if (nameInput.value === '') {
+    if (nameInput.value === "") {
+        nameInput.placeholder = "Your name*"
+        nameInputUnderline.style.width = '100%'
         displayNameRequiredError()
     }
 })
 
 nameInput.addEventListener('input', () => {
-    if (nameInput.value === '') {
+    if (nameInput.value === "") {
+
         displayNameRequiredError()
     } else {
         hideNameRequiredError()
+        const underlineWidth = Number(nameInputUnderline.style.width.substring(0, nameInputUnderline.style.width.length - 2))
+        if (underlineWidth < nameInputContainer.getBoundingClientRect().width) {
+            const fontSize = Number(getComputedStyle(nameInput).fontSize.substring(0, getComputedStyle(nameInput).fontSize.length - 2))
+            nameInputUnderline.style.width = `${nameInput.value.length * fontSize * 0.5}px`
+        }
     }
+
 })
--- src/formInputs/projectInput.ts
+++ src/formInputs/projectInput.ts
@@ -1,35 +1,44 @@
-export const projectInput: HTMLInputElement = document.querySelector('.cc-project-input') as HTMLInputElement
+export const projectInput: HTMLInputElement = document.querySelector('.project-input')
+const projectInputUnderline: HTMLElement = document.querySelector('.project-underline')
+const projectInputContainer: HTMLElement = document.querySelector('.project-input-container')
 export let isProjectErrorDisplayed: boolean = false
-const nameInputBelow: HTMLInputElement = document.querySelector('.cc-name-input') as HTMLInputElement
+const nameInputBelow: HTMLElement = document.querySelector('.cc-name-input')
 const projectRequiredErrorMessage: HTMLElement = document.querySelector('.cc-project-required-error')
 
 const nameInputStyles: CSSStyleDeclaration = getComputedStyle(nameInputBelow)
 const projectRequiredErrorStyles: CSSStyleDeclaration = getComputedStyle(projectRequiredErrorMessage)
 const nameInputMarginTop: string = nameInputStyles.marginTop.substring(0, nameInputStyles.marginTop.length - 2)
 const projectRequiredErrorLineHeight: string = projectRequiredErrorStyles.lineHeight.substring(0, projectRequiredErrorStyles.lineHeight.length - 2)
-
-projectInput.style.border = '1px solid transparent'
-projectInput.style.borderBottom = '1px solid #7b7b7b'
+projectInputUnderline.style.maxWidth = `${projectInputContainer.getBoundingClientRect().width}px`
 
 export const displayProjectRequiredError = (): void => {
 	if (!isProjectErrorDisplayed) {
+		projectInputUnderline.style.width = '100%'
+		projectInputContainer.style.marginBottom = "0px"
 		nameInputBelow.style.marginTop = `${Number(nameInputMarginTop) - Number(projectRequiredErrorLineHeight)}px`
 		projectRequiredErrorMessage.style.display = 'block'
-		projectInput.style.borderBottom = '1px solid #cb0b0b'
+		projectInputUnderline.style.backgroundColor = '#cb0b0b'
 		isProjectErrorDisplayed = true
 	}
 }
 
 const hideProjectRequiredError = (): void => {
-	projectInput.style.border = '1px solid transparent'
-	projectInput.style.borderBottom = '1px solid #7b7b7b'
+	projectInputUnderline.style.backgroundColor = '#7b7b7b'
 	nameInputBelow.style.marginTop = `${nameInputMarginTop}px`
 	projectRequiredErrorMessage.style.display = 'none'
 	isProjectErrorDisplayed = false
 }
 
+projectInput.addEventListener('focus', () => {
+	if (projectInput.value === "") {
+		projectInput.placeholder = ""
+	}
+})
+
 projectInput.addEventListener('blur', () => {
-	if (projectInput.value === '') {
+	if (projectInput.value === "") {
+		projectInput.placeholder = "Tell us about your project*"
+		projectInputUnderline.style.width = '100%'
 		displayProjectRequiredError()
 	}
 })
@@ -39,5 +48,10 @@ projectInput.addEventListener('input', () => {
 		displayProjectRequiredError()
 	} else {
 		hideProjectRequiredError()
+		const underlineWidth = Number(projectInputUnderline.style.width.substring(0, projectInputUnderline.style.width.length - 2))
+		if (underlineWidth < projectInputContainer.getBoundingClientRect().width) {
+			const fontSize = Number(getComputedStyle(projectInput).fontSize.substring(0, getComputedStyle(projectInput).fontSize.length - 2))
+			projectInputUnderline.style.width = `${projectInput.value.length * fontSize * 0.5}px`
+		}
 	}
 })
