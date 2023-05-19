438f3222cb3d66691986044beb93f8c5abf33a9c
dbrosio3 <daniel.brosio@rootstrap.com>
2023-05-19 12:29:30 -0300

    ts fix

--- src/general/theme.ts
+++ src/general/theme.ts
@@ -53,7 +53,7 @@ window.onload = function() {
   themeRootElement.style.transitionDuration = "300ms";
   themeRootElement.style.transitionTimingFunction = "ease";
   const [navBar] = document.querySelectorAll(".c-header-wrapper__light, .c-header-wrapper__dark");
-  if(navBar) {
+  if (navBar instanceof HTMLElement) {
     navBar.style.transitionProperty  = "background-color";
     navBar.style.transitionDuration = "300ms";
     navBar.style.transitionTimingFunction = "ease";
